2140f3ee74e3219eab4cdce39314eb1e0a5ce5a1
CodingMate_Notebook <hsykys0728@gmail.com>
2022-05-06 19:09:19 +0900

    solved 15654

--- src/Baekjoon_backtracking_15654.java
+++ src/Baekjoon_backtracking_15654.java
@@ -1,11 +1,9 @@
-package baekjoon;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Backjoon_backtracking_15654 {
+public class Baekjoon_backtracking_15654 {
 	
 	static StringBuilder result = new StringBuilder();
 	static List<Integer> printStack = new ArrayList<>();
@@ -30,24 +28,26 @@ public class Backjoon_backtracking_15654 {
             else {
                 for ( int j = 0; j < ascSortedNumbers.size(); j++ )
                     if ( number >= ascSortedNumbers.get(j) ){
-                        if ( j == ascSortedNumbers.size() ){
-                            ascSortedNumbers.add(j, number);
+                        if ( j == ascSortedNumbers.size() - 1 ){
+                            ascSortedNumbers.add(number);
+                            break;
                         }
                         else continue;
                     } else {
                         ascSortedNumbers.add(j, number);
+                        break;
                     }
             }
         }
 	    
-	    graph = new int[N+1][N+1];
-	    for ( int fromIndex = 1; fromIndex <= N; fromIndex++ )
-	    	for ( int toIndex = 1; toIndex <= N; toIndex++ )
-                if ( fromIndex <= toIndex )
+	    graph = new int[N][N];
+	    for ( int fromIndex = 0; fromIndex < N; fromIndex++ )
+	    	for ( int toIndex = 0; toIndex < N; toIndex++ )
+                if ( fromIndex != toIndex )
 	    		    graph[fromIndex][toIndex] = 1;
         
 	    
-	    for ( int startIndex = 1; startIndex <= N; startIndex++ )
+	    for ( int startIndex = 0; startIndex < N; startIndex++ )
 	    	dfs(startIndex);
 	    
 	    System.out.print(result);
@@ -57,16 +57,17 @@ public class Backjoon_backtracking_15654 {
 		
 		printStack.add(fromIndex);
 		
-		for ( int toIndex = 1; toIndex <= N; toIndex++ )
+		for ( int toIndex = 0; toIndex < N; toIndex++ )
 			if( printStack.size() < M 
-			 && graph[fromIndex][toIndex] == 1 
+			 && graph[fromIndex][toIndex] == 1
+             && printStack.indexOf(toIndex) == -1
 			  ) {
 				dfs(toIndex);
 			}
 		
 		if ( printStack.size() == M ) {
 			for ( int i = 0; i < printStack.size(); i++ ) {
-				result.append(printStack.get(i) + " ");
+				result.append(ascSortedNumbers.get(printStack.get(i)) + " ");
 			}
 			result.append("\n");
 		}
