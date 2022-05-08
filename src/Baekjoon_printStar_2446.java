dd9f7adf29bc8b7dc37565115644a533f69c6f69
CodingMate_Notebook <hsykys0728@gmail.com>
2022-05-09 07:54:27 +0900

    solved 2446

---
+++ src/Baekjoon_printStar_2446.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_printStar_2446 {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder result = new StringBuilder();
+		for (int row = 1; row <= 2 * N - 1; row++) {
+			String line;
+			if (row <= N) {
+				line = spaces(row - 1) + stars(2 * (N - row) + 1);
+			} else {
+				line = spaces(N - (row - N) - 1) + stars(2 * (row - N) + 1);
+			}
+			result.append(line + "\n");
+		}
+		result.deleteCharAt(result.lastIndexOf("\n"));
+		System.out.print(result);
+	}
+
+	public static String stars(int count) {
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+			result.append("*");
+		}
+		return result.toString();
+	}
+
+	public static String spaces(int count) {
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+			result.append(" ");
+		}
+		return result.toString();
+	}
+}
