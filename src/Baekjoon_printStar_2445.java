bb27299f4cf2ea409157c47fd069c87e46a0873f
CodingMate_Notebook <hsykys0728@gmail.com>
2022-05-09 07:47:58 +0900

    2445

--- src/Baekjoon_printStar_2445.java
+++ src/Baekjoon_printStar_2445.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Baekjoon_printStar_2444 {
+public class Baekjoon_printStar_2445 {
 
 	public static void main(String[] args) throws Exception {
 
