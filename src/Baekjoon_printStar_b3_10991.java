a005c8ee2d0c281ee1c467846756817016fbe371
CodingMate_Notebook <hsykys0728@gmail.com>
2022-05-11 07:13:39 +0900

    solved 10991

---
+++ src/Baekjoon_printStar_b3_10991.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_printStar_b3_10991 {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder result = new StringBuilder();
+		for (int row = 1; row <= N; row++) {
+			String rowLine = spaces(N - row);
+			for (int i = 1; i <= row; i++) {
+				rowLine += stars(1) + spaces(1);
+			}
+			result.append(rowLine + "\n");
+		}
+		result.deleteCharAt(result.lastIndexOf("\n"));
+		System.out.print(result);
+	}
+
+	public static String stars(int count) {
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+			result.append("*");
+		}
+		return result.toString();
+	}
+
+	public static String spaces(int count) {
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+			result.append(" ");
+		}
+		return result.toString();
+	}
+}
