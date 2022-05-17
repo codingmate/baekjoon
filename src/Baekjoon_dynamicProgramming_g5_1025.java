26ed68d719e3fe7521fcb85324294b47a3162082
CodingMate_Notebook <hsykys0728@gmail.com>
2022-05-18 08:19:36 +0900

    1025...... I'm lose....

--- src/Baekjoon_dynamicProgramming_g5_1025.java
+++ src/Baekjoon_dynamicProgramming_g5_1025.java
@@ -31,45 +31,55 @@ class Baekjoon_dynamicProgramming_g5_1025 {
 
         for (int row = 1; row <= N; row++) {
             for (int col = 1; col <= M; col++) {
+
                 if (table[row][col] > maxCompleteNumber && completeNumberList.indexOf(table[row][col]) > -1) {
                     maxCompleteNumber = table[row][col];
                 }
 
-                for (int colD = -M + 1; colD < M; colD++) {
-                    if (colD == 0)
-                        continue;
-                    String rowColMax = Integer.toString(table[row][col]);
-                    for (int i = 1; (col + i * colD) >= 1 && (col + i * colD) <= M; i++) {
-                        rowColMax += Integer.toString(table[row][col + i * colD]);
-                        int rowCol = Integer.parseInt(rowColMax);
-                        if (rowCol > maxCompleteNumber && completeNumberList.indexOf(rowCol) > -1)
-                            maxCompleteNumber = rowCol;
-                    }
-                }
+                // for (int colD = -M + 1; colD < M; colD++) {
+                // if (colD == 0)
+                // continue;
+                // String rowColMax = Integer.toString(table[row][col]);
+                // for (int i = 1; (col + i * colD) >= 1 && (col + i * colD) <= M; i++) {
+                // rowColMax += Integer.toString(table[row][col + i * colD]);
+                // int rowCol = Integer.parseInt(rowColMax);
+                // if (rowCol > maxCompleteNumber && completeNumberList.indexOf(rowCol) > -1)
+                // maxCompleteNumber = rowCol;
+                // }
+                // }
 
-                for (int rowD = -N + 1; rowD < N; rowD++) {
-                    if (rowD == 0)
-                        continue;
-                    String rowColMax = Integer.toString(table[row][col]);
-                    for (int i = 1; (row + i * rowD) >= 1 && (row + i * rowD) <= N; i++) {
-                        rowColMax += Integer.toString(table[row + i * rowD][col]);
-                        int rowCol = Integer.parseInt(rowColMax);
-                        if (rowCol > maxCompleteNumber && completeNumberList.indexOf(rowCol) > -1)
-                            maxCompleteNumber = rowCol;
-                    }
-                }
+                // (int rowD = -N + 1; rowD < N; rowD++) {
+                // rowD == 0)
+                // continue;
+                // String rowColMax = Integer.toString(table[row][col]);
+                // (int i = 1; (row + i * rowD) >= 1 && (row + i * rowD) <= N; i++) {
+                // rowColMax += Integer.toString(table[row + i * rowD][col]);
+                // int rowCol = Integer.parseInt(rowColMax);
+                // rowCol > maxCompleteNumber && completeNumberList.indexOf(rowCol) > -1)
+                // maxCompleteNumber = rowCol;
+                // }
+                // }
 
                 for (int rowD = -N + 1; rowD < N; rowD++) {
                     for (int colD = -M + 1; colD < M; colD++) {
                         if (rowD == 0 && colD == 0)
                             continue;
+
+                        // System.out.println(">>>>>>>>>>>>>");
                         String rowColMax = Integer.toString(table[row][col]);
-                        for (int i = 1; (row + i * rowD) >= 1 && (row + i * rowD) <= N; i++) {
-                            for (int j = 1; (col + j * colD) >= 1 && (col + j * colD) <= M; j++) {
+                        for (int i = 1; i < N && (row + i * rowD) >= 1 && (row + i * rowD) <= N; i++) {
+                            for (int j = 1; j < M && (col + j * colD) >= 1 && (col + j * colD) <= M; j++) {
+
                                 rowColMax += Integer.toString(table[row + i * rowD][col + j * colD]);
                                 int rowCol = Integer.parseInt(rowColMax);
-                                if (rowCol > maxCompleteNumber &&
-                                        (rowCol % 10 == 0 ||
+                                System.out.print((rowD) + ", " + (colD) + " : ");
+                                System.out.print((i) + ", " + (j) + " : ");
+
+                                System.out.println((row + i * rowD) + ", " + (col + j * colD) + " : " +
+                                        rowCol);
+                                // System.out.println(rowCol);
+                                if (rowCol > maxCompleteNumber
+                                        && (rowCol % 10 == 0 ||
                                                 rowCol % 10 == 1 ||
                                                 rowCol % 10 == 4 ||
                                                 rowCol % 10 == 5 ||
@@ -77,6 +87,8 @@ class Baekjoon_dynamicProgramming_g5_1025 {
                                                 rowCol % 10 == 9)
                                         && completeNumberList.indexOf(rowCol) > -1)
                                     maxCompleteNumber = rowCol;
+                                if (colD == 0)
+                                    break;
                             }
                         }
 
