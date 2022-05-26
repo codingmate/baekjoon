fdde94c6b5163e730fd493c5c5455ce1c5ea77a8
계정 <hsykys0728@gmail.com>
2022-05-27 06:29:07 +0900

    solved 1463. Hard & NS!!!!

--- src/Baekjoon_dynamicProgramming_s3_1463.java
+++ src/Baekjoon_dynamicProgramming_s3_1463.java
@@ -4,21 +4,30 @@ class Baekjoon_dynamicProgramming_s3_1463 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N;
-    static int executeCount = 0;
+    static int minExectueCount = 0;
+    static int[] counts;
+
     public static void main(String[] args) throws Exception {
         N = Integer.parseInt(br.readLine());
-        
-        
-            
-            while( N > 1 ){
 
-            }
-        
-        
-        
-        
+        counts = new int[N+1];
+        counts[0] = 0;
+        counts[1] = 0;
 
-        System.out.print(executeCount);
+        for ( int i = 2; i <= N; i++ ){
+            counts[i] = counts[i-1] + 1;
+            if ( i % 2 == 0 ) counts[i] = counts[i] < (counts[i/2] + 1)
+                                               ? counts[i]
+                                               : (counts[i/2] + 1);
+            if ( i % 3 == 0 ) counts[i] = counts[i] < (counts[i/3] + 1)
+                                               ? counts[i]
+                                               : (counts[i/3] + 1);
+
+            System.out.printf("counts[%d] : %d\n", i, counts[i]);
+                                               
+        }
+        br.close();
+        System.out.print(counts[N]);
     }
 
     
