7b9418f2825e9816d4e244b4afa7921be65452f7
sy-playground <87705358+codingmate@users.noreply.github.com>
2022-05-26 12:43:29 +0900

    solved 14916

---
+++ src/src/Baekjoon_dynamicProgramming_s5_14916.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int minCoinCount = Integer.MAX_VALUE;
+    
+    public static void main(String[] args) throws Exception {
+        
+        n = Integer.parseInt(br.readLine());
+        int coin5Count = 0;
+        int coin2Count = 0;
+        int coinCount = 0;
+        int copyN = n;
+        boolean completeTF = false;
+        for ( coin5Count = 0; coin5Count <= n / 5; coin5Count++ ){
+            copyN = n;
+            copyN -= coin5Count * 5;
+
+            if( copyN % 2 == 0 ){
+                completeTF = true;
+                coin2Count = copyN / 2;
+                coinCount = coin5Count + coin2Count;
+                minCoinCount = minCoinCount > coinCount ? coinCount : minCoinCount;
+            }
+        }
+        if ( completeTF ){
+            System.out.print(minCoinCount);
+        } else {
+            System.out.print(-1);
+        }
+    }
+}
