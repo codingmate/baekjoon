3e39a4953dd546a36b151270b10e379e1b845e61
sy-playground <87705358+codingmate@users.noreply.github.com>
2022-05-26 13:01:38 +0900

    ing 13699

---
+++ src/Baekjoon_dynamicProgramming_s4_13699.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+class Baekjoon_dynamicProgramming_s4_13699 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static List < Long > valueList = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
+        
+        n = Integer.parseInt(br.readLine());
+        valueList.add(1l);
+        for ( int i = 1; i < n; i++ ){
+            valueList.add(valueSetting(i));
+        }
+        for ( Long value : valueList ){
+            System.out.println(value);
+        }
+    }
+
+    public static long valueSetting (int num) {
+        Long value = 0l;
+        for ( int i = 0; i < num; i++ ){
+            value += valueList.get(i) * valueList.get(num-i);
+        }
+        return value;
+    }
+    
+}
