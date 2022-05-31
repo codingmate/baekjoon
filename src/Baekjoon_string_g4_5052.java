28835dc2bb055bab8ae335b7f73a1603faa8799d
계정 <hsykys0728@gmail.com>
2022-05-31 20:30:32 +0900

    ing 5052.. difficult..

---
+++ src/Baekjoon_string_g4_5052.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Baekjoon_string_g4_5052 {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        for ( int r = 0; r < t; r++ ){
+            List<String> strList = new ArrayList<>();
+            for ( int e = 0; e < n; e++ ){
+                strList.add(br.readLine());
+            }
+            
+            for ( int i = 0; i < 10; i++ ){
+                
+            }
+        }
+    }
+}
