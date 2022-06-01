d1ab3d95331598d4b8341a6eaff4962a97f2c845
계정 <hsykys0728@gmail.com>
2022-06-01 23:34:38 +0900

    solved 1152

--- src/Baekjoon_string_b2_1152.java
+++ src/Baekjoon_string_b2_1152.java
@@ -6,6 +6,7 @@ public class Baekjoon_string_b2_1152 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
         int result = line.trim().split(" ").length;
+        if( line.trim().equals("") ) result = 0;
         System.out.print(result);
     }
 }
