224419d7b8f71031d83a3ca9506ce6fd2a9babef
계정 <hsykys0728@gmail.com>
2022-06-06 21:35:18 +0900

    faled 15740

--- src/Baekjoon_io_b5_15740.java
+++ src/Baekjoon_io_b5_15740.java
@@ -11,113 +11,137 @@ public class Baekjoon_io_b5_15740 {
     public static void main(String[] args) throws IOException {
 
         String[] AB = br.readLine().split(" ");
-        
-        boolean AisNegative = false;
-        boolean BisNegative = false;
         String A = AB[0];
         String B = AB[1];
-        /* 마이너스 부호 제거 및 A, B 음수 판별 */
-        if ( A.charAt(0) == '-' ){
-            A = A.substring(1);
+
+        boolean AisNegative = false;
+        boolean BisNegative = false;
+        /* A (-) 부호 제거 */
+        if ( A.charAt(0) == '-' ) {
             AisNegative = true;
+            A = A.substring(1);
         }
-        if ( B.charAt(0) == '-' ){
-            B = B.substring(1);
+        /* B (-) 부호 제거 */
+        if ( B.charAt(0) == '-'  ) {
             BisNegative = true;
+            B = B.substring(1);
         }
 
-        /* A와 B의 길이를 맞추기 위한 보정 작업 ( 앞을 0으로 채움) */
+
+        /* A와 B의 절댓값의 자릿수 맞춰주기 ( 앞을 0으로 채우기 ) */
         int diffLength = A.length() - B.length();
         if ( diffLength > 0 ){
             for ( int i = 0; i < diffLength; i++ ){
                 B = "0" + B;
             }
         } else if ( diffLength < 0 ){
-            for ( int i = 0; i < diffLength; i++ ){
+            for ( int i = 0; i < (-1 * diffLength); i++ ){
                 A = "0" + A;
             }
-        }
+        }  
+        /* A와 B의 절댓값 중 누가 더 큰지 판별 */
+        boolean AIsGreaterThanB = false;
+        boolean BIsGreaterThanA = false;
 
-        /* A와 B의 절댓값 비교 -> A가 B보다 큰지 판별 */
-        boolean AisGreaterThanB = false;
-        boolean AEqualB = false;
-        for ( int i = A.length()-1; i > -1; --i ){
+        for ( int i = 0; i < A.length(); i++ ){
             Character charA = A.charAt(i);
             Character charB = B.charAt(i);
+
             int a = Integer.parseInt(charA.toString());
             int b = Integer.parseInt(charB.toString());
 
             if ( a > b ) {
-                AisGreaterThanB = true;
+                AIsGreaterThanB = true;
                 break;
-            } else if ( a < b ){
-                AisGreaterThanB = false;
+            } else if ( b > a ) {
+                BIsGreaterThanA = true;
                 break;
-            } else if (i == 0) { /* a == b */
-                AEqualB = true;
-            } 
+            } else if ( i == A.length() - 1 ) {
+                /* 둘이 같음 */
+            }
         }
-        
-        /* 각 자릿 수의 연산 결과를 저장하는 리스트 (가장 오른쪽 자릿수부터 저장) */
+
+        /* 각 자리 숫자를 연산한 결과를 갖는 List */
         List <Integer> sumList = new ArrayList<>();
-        /* 올림 수로 하나의 자릿수 추가 */
+
+        /* 최대 자릿수 증가 가능성으로, 자릿수 하나 더 추가 */
         sumList.add(0);
-        /* A, B 둘 다 양수 또는 둘 다 음수일 때 */
-        if ( AisNegative = BisNegative ) {
 
-            /* 두 절댓값을 더하는 연산 */
-            for ( int i = A.length()-1; i > -1; i-- ){
-
+        if ( AisNegative == BisNegative ){
+            /* 1. A, B의 부호가 같을 때 각 자릿수 더해서 sumList에 넣기 */
+            for ( int i = 0; i < A.length(); i++ ) {
                 Character charA = A.charAt(i);
                 Character charB = B.charAt(i);
+
                 int a = Integer.parseInt(charA.toString());
                 int b = Integer.parseInt(charB.toString());
-                int sum = a+b;
+
+                sumList.add(a+b);
+            }
+            /* 각 자릿수가 10 이상이면 위로 올린다. */
+            for ( int i = A.length(); i > -1; i-- ) {
+                int sum = sumList.get(i);
+                if ( sum >= 10 ) {
+                    sumList.set( i - 1, sumList.get(i-1) + 1 );
+                    sumList.set( i, sum - 10);
+                }
+            }
+        } else {
+            /* 2. A, B의 부호가 다를 때 */
+            for ( int i = 0; i < A.length(); i++ ) {
+                Character charA = A.charAt(i);
+                Character charB = B.charAt(i);
+
+                int a = Integer.parseInt(charA.toString());
+                int b = Integer.parseInt(charB.toString());
+
+                int sum = AIsGreaterThanB ? (a-b) : (b-a);
                 sumList.add(sum);
             }
-            /* 각 자릿수의 합이 10이 넘을 경우, 올림수 반영 */
-            for ( int i = 0; i < sumList.size(); i++ ) {
+            /* 각 자릿수가 0보다 작으면 위로 내린다. */
+            for ( int i = A.length(); i > -1; i-- ) {
                 int sum = sumList.get(i);
-                if ( sum > 10) {
-                    sumList.set(i+1, sumList.get(i+1) + 1);
-                    sumList.set(i, sum - 10);
-                    //System.out.println("i : " + i + ", " + sumList.get(i) + " " + sumList.get(i+1));
+                if ( sum < 0 ) {
+                    sumList.set( i - 1, sumList.get(i-1) - 1 );
+                    sumList.set( i, sum + 10);
                 }
             }
         }
-        /* A와 B중 하나만 음수일 때 */
-        else {
-            /* 두 절댓값에서, 큰 값에서 작은 값 뺴는 연산 */
-            for ( int i = A.length()-1; i > -1; i-- ){
-
-                Character charA = A.charAt(i);
-                Character charB = B.charAt(i);
-                int a = Integer.parseInt(charA.toString());
-                int b = Integer.parseInt(charB.toString());
-                int sum = AisGreaterThanB ? a - b : b - a;
-                sumList.add(sum);
-            }
-            /* 각 자릿수의 합이 0보다 작을 경우, 내림수 반영 */
-            for ( int i = sumList.size()-1; i > -1; i-- ) {
-                int sum = sumList.get(i);
-                if ( sum < 0) {
-                    sumList.set(i+1, sumList.get(i+1) - 1);
-                    sumList.set(i, sum + 10);
-                    //System.out.println("i : " + i + ", " + sumList.get(i) + " " + sumList.get(i+1));
-                }
-            }
-        }
+        /* 최대 자릿수 증가를 대비하여 넣은 값이 0이면 삭제 */
+        //if (sumList.get(0) == 0)
+        //    sumList.remove(0);
         
         StringBuilder result = new StringBuilder();
-        if ( (AisGreaterThanB && AisNegative) || (!AisGreaterThanB )){
-            result.append("-");
-        } 
-        if ( sumList.get(sumList.size()-1) == 0 )
-            sumList.remove(sumList.size()-1);
-        for ( int i = sumList.size()-1; i > -1; i-- ){
+        int firstNot0Index = 0;
+        for ( int i = 0; i < sumList.size(); ++i ){
+            int sum = sumList.get(i);
+            if ( sum > 0 && firstNot0Index == 0 ){
+                firstNot0Index = i;
+            }
+            if (sum == 0 && firstNot0Index == 0) continue;
             result.append(sumList.get(i));
         }
         
-        System.out.println(result.toString());
+        if ( result.length() == 0 ) {
+            result.append("0");
+        }
+
+        if( ( AisNegative == true 
+           && BisNegative == false
+           && AIsGreaterThanB == true) 
+         || ( BisNegative == true
+           && AisNegative == false
+           && BIsGreaterThanA == true
+            )
+         || ( BisNegative == true 
+           && AisNegative == true
+            )
+          ){
+            System.out.print( "-" + result.toString());
+        } else 
+            System.out.print( result.toString() );
+        
+        
+        
     }
 }
