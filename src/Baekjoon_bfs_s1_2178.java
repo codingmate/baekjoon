995bc608c467c19e9a9b1e9fc79417492007a083
계정 <hsykys0728@gmail.com>
2022-06-16 23:06:43 +0900

    ing 2178... diff bfs T.T

--- src/Baekjoon_bfs_s1_2178.java
+++ src/Baekjoon_bfs_s1_2178.java
@@ -10,7 +10,7 @@ public class Baekjoon_bfs_s1_2178 {
     static int M = 0;
     static int[][] graph;
     static boolean[][] visited;
-    static List<String> queue = new LinkedList<>();
+    static List<Point> queue = new LinkedList<>();
     public static void main(String[] args) throws Exception {
         String[] NM = br.readLine().split(" ");
         N = Integer.parseInt(NM[0]);
@@ -21,53 +21,74 @@ public class Baekjoon_bfs_s1_2178 {
             String[] numbers = br.readLine().split("");
             for ( int col = 0; col < M; col++ ) {
                 graph[row][col] = Integer.parseInt(numbers[col]);
+                if ( graph[row][col] == 1 ) 
+                    visited[row][col] = true;
             }
         }
-
-        for ( int row = 0; row < N; row++ ) {
-            for (int col = 0; col < M; col++ ){
-                System.out.print(graph[row][col] + " ");
-            }
-            System.out.println();
-        }
-
-        bfs(0, 0);
-    }
-
-    public static void bfs( int fromX, int fromY ) {
-        visited[fromX][fromY] = true;
         
-        if ( fromY > 0 && !visited[fromX][fromY-1] ) {
-            queue.add(set(fromX, fromY-1));
-            visited[fromX][fromY-1] = true;
-        }
-        if ( fromY < N - 1 && !visited[fromX][fromY+1] ) {
-            queue.add(set(fromX, fromY+1));
-            visited[fromX+1][fromY] = true;
-        }
-        if ( fromX > 0 && !visited[fromX-1][fromY] ) {
-            queue.add(set(fromX-1, fromY));
-            visited[fromX-1][fromY] = true;
-        }
-        if ( fromX < M - 1 && !visited[fromX+1][fromY] ) {
-            queue.add(set(fromX+1, fromY));
-            visited[fromX+1][fromY] = true;
-        }
-
+        Point temp = new Point(0, 0);
+        bfs(temp);
         while( queue.size() > 0 ) {
-            int[] XY = get(queue.get(0));
-            bfs(XY[0], XY[1]);
-            queue.remove(queue.size() - 1);    
+            Point p = queue.get(0);
+            bfs(p);
+            System.out.printf("x : %d, y : %d\n", p.row, p.col);
+            queue.remove(0);
+        }
+        
+    }
+
+    public static void bfs( Point p ) {
+        visited[p.row][p.col] = true;
+        p.insertNextPoint();
+        for ( Point po : queue ) {
+            System.out.println(po.row + " " + po.col);
         }
     }
-    public static String set(int a, int b) {
-        return a + "\\" + b;
-    }
+    
+    
+    public static class Point {
+        public int row;
+        public int col;
+        public Point( int row, int col ) {
+            this.row = row;
+            this.col = col;
+        }
+        public boolean insertNextPoint() {
 
-    public static int[] get(String xy) {
-        String[] X_Y = xy.split("\\");
-        int[] XY = { Integer.parseInt(X_Y[0]), Integer.parseInt(X_Y[1]) };
-
-        return XY;
+            System.out.printf("x : %d, y : %d, %b\n", row-1, col, row > 0 ? visited[row-1][col] : false);
+            System.out.printf("x : %d, y : %d, %b\n", row+1, col, row < N-1 ? visited[row+1][col] : false);
+            System.out.printf("x : %d, y : %d, %b\n", row, col+1, col < M-1 ? visited[row][col+1] : false);
+            System.out.printf("x : %d, y : %d, %b\n", row, col-1, col > 0 ? visited[row][col-1] : false);
+            System.out.println(queue.size());
+            if ( col > 0 ? visited[row][col-1] : true
+              && col < M-1 ? visited[row][col+1] : true
+              && row < N-1 ? visited[row+1][col] : true
+              && row > 0 ? visited[row-1][col] : true) {
+                return false;
+            } else {
+                
+                if ( row > 0 
+                && graph[row-1][col] == 1 
+                && !visited[row-1][col]  ) {
+                    queue.add(new Point(row-1, col));
+                }
+                if ( row < N - 1 
+                && graph[row+1][col] == 1 
+                && !visited[row+1][col] ) {
+                    queue.add(new Point(row+1, col));
+                }
+                if ( col > 0 
+                && graph[row][col-1] == 1 
+                && !visited[row][col-1] ) {
+                    queue.add(new Point(row, col-1));                    
+                }
+                if ( col < M - 1 
+                && graph[row][col+1] == 1 
+                && !visited[row][col+1] ) {
+                    queue.add(new Point(row, col+1));
+                }
+            }
+            return true;
+        }
     }
 }
