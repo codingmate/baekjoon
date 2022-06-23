d3b2f801f86180ca234d613aa9ff573c1a8a2a9a
sy-playground <87705358+codingmate@users.noreply.github.com>
2022-06-23 12:43:19 +0900

    ing 1181 / need string sort (length, asc)

---
+++ src/Baekjoon_string_s5_1181.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+public class Baekjoon_string_s5_1181 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static List<String> wordList = new ArrayList<>();
+    static Set<String> wordSet = new HashSet<>();
+    static String[][] wordSetArray;
+    public static void main(String[] args) throws Exception {
+        
+        N = Integer.parseInt(br.readLine());
+        
+        for ( int i = 0; i < N; i++ ) {
+            String word = br.readLine();
+            if ( !wordSet.contains(word) ) {
+                wordSet.add(word);
+                wordList.add(word);
+            }
+        }
+        
+        System.out.println(wordList)
+        
+                
+        
+        br.close();
+    }
+
+}
