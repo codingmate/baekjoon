b7d1e9ff12072aa1e289317e8b41f442e975db03
sy-playground <87705358+codingmate@users.noreply.github.com>
2022-06-28 12:59:40 +0900

    ing2  7576 little bit..

--- src/Baekjoon_bfs_g5_7576.java
+++ src/Baekjoon_bfs_g5_7576.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
  
 class Baekjoon_bfs_g5_7576 {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N;
     static int M;
     static int[][] graph;
@@ -14,37 +14,44 @@ class Baekjoon_bfs_g5_7576 {
     static List<Dot> startDotList = new ArrayList<>();
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    
     public static void main(String[] args) throws Exception {
         String[] NM = br.readLine().split(" ");
-        N = Integer.parseInt(NM[0]);
-        M = Integer.parseInt(NM[1]);
+        N = Integer.parseInt(NM[1]);
+        M = Integer.parseInt(NM[0]);
 
+        graph = new int[N][M];
         for ( int i = 0 ; i < N; i++ ) {
             String[] row = br.readLine().split(" ");
-            for ( int j = 0 ; j < M; j++ )
-                if( row[j].equals("1") ) {
-                    graph[i][j] = 1;
+            for ( int j = 0 ; j < M; j++ ) {
+                graph[i][j] = Integer.parseInt(row[j]);
+                if( graph[i][j] == 1 )
                     startDotList.add(new Dot(i, j));
-                }
+            }
+                
         }
 
-        int count = 0;
-        for ( int i = 0; startDotList.size(); i++ ) {
+        
+        for ( int i = 0; i < startDotList.size(); i++ ) {
             bfs(startDotList.get(i));
+            while( queue.size() > 0 ) {
+                Dot d = queue.get(0);
+                queue.remove(0);
+                bfs(d);
+            }
+            
+            for ( int[] row : graph ) {
+                for ( int col : row ) {
+                    System.out.print(col + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("\n\n");
         }
 
-        
-        while( queue.size() > 0 ) {
-            Dot d = queue.get(0);
-            queue.remove(0);
-            bfs(d, count);
-        }
-        
     } // main
 
-    static void bfs( Dot d, int count ) {
-
-        graph[d.row][d.col] = count;
+    static void bfs( Dot d ) {
         for ( int i = 0; i < 4; i++ ) {
             int nextRow = d.row + dx[i];
             int nextCol = d.col + dy[i];
@@ -52,7 +59,12 @@ class Baekjoon_bfs_g5_7576 {
               && nextRow < N
               && nextCol > -1
               && nextCol < M
-              && graph[nextRow][nextCol] < count) {
+              && ( graph[nextRow][nextCol] == 0
+                || ( graph[nextRow][nextCol] != -1 
+                  && graph[nextRow][nextCol] > graph[d.row][d.col] + 1)
+                 )
+               ) {
+                graph[nextRow][nextCol] = graph[d.row][d.col] + 1;
                 queue.add(new Dot(nextRow, nextCol));
               }
         }
