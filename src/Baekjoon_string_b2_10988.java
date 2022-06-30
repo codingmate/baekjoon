b1771d4715beea0dfdeff0f978dde48cbd200cdd
계정 <hsykys0728@gmail.com>
2022-06-30 22:05:10 +0900

    solved 10988

--- src/Baekjoon_string_b2_10988.java
+++ src/Baekjoon_string_b2_10988.java
@@ -1,5 +1,22 @@
 package src;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class Baekjoon_string_b2_10988 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main ( String [] args ) throws Exception {
+
+        String word = br.readLine();
+        boolean result = true;
+        for (int i = 0; i < word.length()/2; i++ ) {
+            if ( word.charAt(i) != word.charAt(word.length()-1 - i) ) {
+                result = false;
+                break;
+            }
+        }
+
+        System.out.print(result ? 1 : 0);
+    }
     
 }
