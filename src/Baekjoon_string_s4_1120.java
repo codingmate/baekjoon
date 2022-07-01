5edbb348ef8f6da5288e0d08ebe5e786350a0770
sy-playground <87705358+codingmate@users.noreply.github.com>
2022-07-01 19:48:15 +0900

    ing 1120

---
+++ src/Baekjoon_string_s4_1120.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Baekjoon_string_s4_1120 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception{
+        String[] AB = br.readLine().split(" ");
+        String A = AB[0];
+        String B = AB[1];
+
+        int diff = 0;
+    }
+}
