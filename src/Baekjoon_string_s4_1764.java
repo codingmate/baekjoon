59869c18ab812b3425f596a0a0203e7bec69e922
codingmate <hsykys0728@gmail.com>
2022-07-02 11:34:35 +0900

    solved 1764. using TreeSet&HashSet

--- src/Baekjoon_string_s4_1764.java
+++ src/Baekjoon_string_s4_1764.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.TreeSet;
-import java.util.List;
-import java.util.ArrayList;
 public class Baekjoon_string_s4_1764 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,23 +10,27 @@ public class Baekjoon_string_s4_1764 {
         String[] NM = br.readLine().split(" ");
         int N = Integer.parseInt(NM[0]);
         int M = Integer.parseInt(NM[1]);
-        int count = 0;
-        Set<String> cantHearSet = new TreeSet<>();
-        List<String> cantHearSeeList = new ArrayList<>();
+        
+        Set<String> cantHearSet = new HashSet<>();
+        Set<String> cantHearSeeSet = new TreeSet<>();
+        /* 듣도 못한 사람들 */
         for ( int row = 0; row < N; row++ )
             cantHearSet.add(br.readLine());
+            
+        /* 듣도 보도 못한 사람들 */
         for ( int row = 0; row < M; row++ ){
             String key = br.readLine();
-            if ( cantHearSet.contains(key) ) {
-                count++;
-                cantHearSeeList.add(key);
+            if ( cantHearSet.contains(key) ) {                
+                cantHearSeeSet.add(key);
             }
         }
-
+        System.out.println(cantHearSeeSet.size());
         /* cantHearSeeList 문자열 오름차순 정렬 필요 */
-        
-        for ( String s : cantHearSeeList ) {
+        for ( String s : cantHearSeeSet ) {
             result.append( s + "\n" );
         }
+        if ( result.length() > 1 )
+            result.deleteCharAt(result.length()-1);
+        System.out.print(result.toString());
     }
 }
