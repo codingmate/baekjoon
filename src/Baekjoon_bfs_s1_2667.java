0ebe419f382f4ccc407edc81f2a1d212fa504ec1
codingmate <hsykys0728@gmail.com>
2022-07-06 21:39:19 +0900

    sovled 2667

--- src/Baekjoon_bfs_s1_2667.java
+++ src/Baekjoon_bfs_s1_2667.java
@@ -1,93 +1,88 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
-
+import java.util.*;
+import java.io.*;
 public class Baekjoon_bfs_s1_2667 {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N = 0;
-    static int M = 0;
-    static int[][] graph;
-    static int[] deltaRow = {-1, 1, 0, 0};
-    static int[] deltaCol = {0, 0, -1, 1};
-    static Queue<Point> queue = new LinkedList<>();
-    static int cost = 1;
-    static HashMap<Integer, Integer> costCountsMap = new HashMap<Integer, Integer>();
+
+    static int N;
+    static int[][] matrix;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    static List<Dot> first1DotList = new LinkedList<>();
+    static List<Dot> queue = new LinkedList<>();
+    static int[] dx = { 1, -1, 0, 0 };
+    static int[] dy = { 0, 0, 1, -1 };
     public static void main(String[] args) throws Exception {
-        String[] NM = br.readLine().split(" ");
-        
-        N = Integer.parseInt(NM[0]);
-        M = N;
-        graph = new int[N][M];
+
+        N = Integer.parseInt(br.readLine());
+        matrix = new int[N][N];
         for ( int row = 0; row < N; row++ ) {
-            String[] numbers = br.readLine().split("");
-            for ( int col = 0; col < M; col++ ) {
-                graph[row][col] = Integer.parseInt(numbers[col]);
+            String[] splitLine = br.readLine().split("");
+            for ( int col = 0; col < N; col++ ) {
+                matrix[row][col] = Integer.parseInt(splitLine[col]);
+                if ( matrix[row][col] == 1 )
+                    first1DotList.add(new Dot(row, col));
             }
         }
-
-        Point first1Point;
-        while( (first1Point = getFirstNPoint(1)) != null  ) {
-            cost++;
-            bfs(first1Point);
-            while( !queue.isEmpty() ) {
-                bfs(queue.poll());
+        int value = 1;
+        while( first1DotList.size() > 0 ) {
+            Dot startDot = first1DotList.get(0);
+            if( matrix[startDot.row][startDot.col] == 1) {
+                value++;
+                matrix[startDot.row][startDot.col] = value;
+                queue.add(startDot);
+                bfs(value);
             }
+            first1DotList.remove(0);
+        }
+
+        
+        List<Integer> countList = new ArrayList<>();
+        for ( int i = 2; i <= value; i++ ) {
+            int count = 0;
+            for ( int row = 0; row < N; row++ )
+                for ( int col = 0; col < N; col++ )
+                    if ( matrix[row][col] == i )
+                        count++;
+            countList.add(count);
+        }
+        
+        // for ( int[] line : matrix) {
+        //     for ( int element : line) {
+        //         System.out.print(element + " ");
+        //     }
+        //     System.out.println();
+        // }
+        StringBuilder result = new StringBuilder();
+        result.append((value-1) + "\n");
+        Collections.sort(countList);
+        for (int count : countList )
+            result.append(count + "\n");
+        System.out.print(result.toString());
+        
+    }
+    
+    public static void bfs(int value) {
+        while( queue.size() > 0 ) {
+
+            Dot d = queue.get(0);
+            queue.remove(0);
             
-        }
-        
-        System.out.println(cost-1);
-        
-        Set<Integer> costCountSet = new TreeSet<>();
-
-        for ( Integer e : costCountsMap.values() ) {
-            costCountSet.add(e);
-        }
-
-        /* Selection Sort ASC*/
-        for ( int e : costCountSet ) {
-            System.out.println(e);
-        }
-
-        br.close();
-
-    }
-
-    public static Point getFirstNPoint ( int n ) {
-        
-        for ( int row = 0; row < N; row++ )
-            for ( int col = 0; col < M; col++ )
-                if ( graph[row][col] == n) return new Point(row, col);
-        return null;
-    }
-
-    public static void bfs( Point p ) {
-        
-        for ( int i = 0; i < deltaCol.length; i++ ) {
-            int nextRow = p.row + deltaRow[i];
-            int nextCol = p.col + deltaCol[i];
-            if ( nextRow > -1 && nextRow < N
-              && nextCol > -1 && nextCol < M ) {
-                if ( graph[nextRow][nextCol] == 1 ) {
-                    graph[nextRow][nextCol] = cost;
-                    costCountsMap.put( cost
-                                     , costCountsMap.containsKey(cost) 
-                                     ? (costCountsMap.get(cost) + 1)
-                                     : 1 );
-                    queue.add(new Point(nextRow, nextCol));
-                }    
+            for ( int i = 0; i < 4; i++ ) {
+                int nextRow = d.row + dx[i];
+                int nextCol = d.col + dy[i];
+                if ( nextRow > -1 && nextRow < N
+                  && nextCol > -1 && nextCol < N
+                  && matrix[nextRow][nextCol] == 1
+                   ) {
+                    matrix[nextRow][nextCol] = value;
+                    queue.add(new Dot(nextRow, nextCol));
+                  }
             }
         }
+            
     }
-        
-    public static class Point {
+    public static class Dot {
         public int row;
         public int col;
-        public Point( int row, int col ) {
+        public Dot(int row, int col) {
             this.row = row;
             this.col = col;
         }
