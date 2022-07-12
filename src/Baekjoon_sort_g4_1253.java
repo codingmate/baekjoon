64d2c1c0e8e7fd9bbf2f324ace15b32778aec22f
계정 <hsykys0728@gmail.com>
2022-07-12 21:08:29 +0900

    76% failed..!!!

--- src/Baekjoon_sort_g4_1253.java
+++ src/Baekjoon_sort_g4_1253.java
@@ -12,12 +12,13 @@ public class Baekjoon_sort_g4_1253 {
         Set<Long> sumSet = new HashSet<>();
         for ( String num : nums ) {
             numList.add(Long.parseLong(num));
-            for ( int i = 0; i < numList.size(); i++ ) {
-                Long numI = numList.get(i);
-                for ( int j = i + 1; j < numList.size(); j++ ) {
-                    if ( sumSet.contains(numI + numList.get(j)) )
-                        sumSet.add(numI + numList.get(j));
-                }
+        }
+
+        for ( int i = 0; i < numList.size(); i++ ) {
+            Long numI = numList.get(i);
+            for ( int j = i + 1; j < numList.size(); j++ ) {
+                if ( !sumSet.contains(numI + numList.get(j)) )
+                    sumSet.add(numI + numList.get(j));
             }
         }
 
