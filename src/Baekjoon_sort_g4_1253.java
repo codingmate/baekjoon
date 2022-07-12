d3fea2a4bc0e7a9569e7107403c7045f1e7ef7bd
계정 <hsykys0728@gmail.com>
2022-07-12 21:26:02 +0900

    3% timeover... 1253

--- src/Baekjoon_sort_g4_1253.java
+++ src/Baekjoon_sort_g4_1253.java
@@ -9,23 +9,32 @@ public class Baekjoon_sort_g4_1253 {
         int N = Integer.parseInt(br.readLine());
         String[] nums = br.readLine().split(" ");
         List<Long> numList = new ArrayList<>();
-        Set<Long> sumSet = new HashSet<>();
         for ( String num : nums ) {
             numList.add(Long.parseLong(num));
         }
-
+        int count = 0;
         for ( int i = 0; i < numList.size(); i++ ) {
             Long numI = numList.get(i);
-            for ( int j = i + 1; j < numList.size(); j++ ) {
-                if ( !sumSet.contains(numI + numList.get(j)) )
-                    sumSet.add(numI + numList.get(j));
-            }
-        }
-
-        int count = 0;
-        for ( long num : numList )
-            if ( sumSet.contains(num) )
-                count++;
+            boolean isCounted = false;
+            for ( int j = 0; j < numList.size(); j++) {
+                if ( i == j )
+                    continue;
+                else {
+                    Long numJ = numList.get(j);
+                    for ( int k = j + 1; k < numList.size(); k++ ) {
+                        if ( i != k && (numI == numJ + numList.get(k)) ) {
+                            count++;
+                            //System.out.printf("numI : %d, numJ : %d, num : %d \n", numI, numJ, numList.get(k));
+                            isCounted = true;
+                            break;
+                        }
+                    } //for : k
+                } // else
+                if ( isCounted )
+                    break;
+            } // for : j
+        } // for : i
+        
         System.out.print(count);
         
     }
