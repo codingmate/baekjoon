52d55c14f51c0de03bd7b1a33352408bdeba6891
sy-playground <87705358+codingmate@users.noreply.github.com>
2022-07-25 12:39:15 +0900

    failed 8956

--- src/Baekjoon_string_b2_8958.java
+++ src/Baekjoon_string_b2_8958.java
@@ -9,13 +9,13 @@ public class Baekjoon_string_b2_8958 {
         StringBuilder result = new StringBuilder();
         for (int row = 0; row < N; row++) {
             String line = br.readLine();
-            int start = 0;
-            int end = 0;
+            int start = -1;
+            int end = -1;
             int lineScore = 0;
-            System.out.println();
+            //System.out.println();
             for (int col = 0; col < line.length(); col++) {
-                char c = line.charAt(col);
                 end++;
+                char c = line.charAt(col);
                 switch (c) {
                     case 'O':
                         break;
@@ -27,12 +27,14 @@ public class Baekjoon_string_b2_8958 {
                                 score += i;
                             lineScore += score;
                             start = end;
-                            System.out.print(score + " ");
+                            //System.out.print(score + " ");
+                        } else if ( col > 0 && line.charAt(col-1) == 'X' ) {
+                            start++;
                         }
                         break;
                     default:
                         break;
-                }
+                } // switch
 
             }
             
@@ -42,11 +44,13 @@ public class Baekjoon_string_b2_8958 {
                     for (int i = 1; i <= diff; i++)
                         score += i;
                     lineScore += score;
-                System.out.print("!" + score + " ");
+                //System.out.print("!" + score + " ");
                 }
-            System.out.printf("lineScore : %d\n", lineScore);
+            //System.out.printf("lineScore : %d\n", lineScore);
             result.append(lineScore + "\n");
         }
+        if ( result.length() > 0 )
+            result.deleteCharAt(result.length()-1);
         System.out.print(result.toString());
 
     } // main
